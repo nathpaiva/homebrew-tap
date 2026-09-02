d1215323f181f73f812a1f0d7f1a65c29e8adaad
Nath Paiva <hello@nathpaiva.com.br>
2026-09-02 16:08:01 -0700

    Add bru-run 1.0.0

---
+++ Formula/bru-run.rb
@@ -0,0 +1,35 @@
+class BruRun < Formula
+  desc "Run Bruno collection requests from any project, from any directory"
+  homepage "https://github.com/nathpaiva/bru-run"
+  url "https://github.com/nathpaiva/bru-run/archive/refs/tags/v1.0.0.tar.gz"
+  sha256 "10695438165863978be8368a2ce3323947ae6778b75d1357db006c2b80975bfb"
+  license "MIT"
+
+  depends_on "bash"
+  depends_on "jq"
+
+  def install
+    libexec.install "lib"
+    libexec.install "bin/bru-run"
+    # Pin the shebang to Homebrew's bash so macOS system bash 3.2 is never
+    # hit: bru-run needs 4.0+ and fails fast otherwise.
+    inreplace libexec/"bru-run", %r{^#!/usr/bin/env bash$}, "#!#{formula_opt_bin("bash")}/bash"
+    bin.install_symlink libexec/"bru-run"
+  end
+
+  def caveats
+    <<~EOS
+      bru-run sends requests through the Bruno CLI, which is not in Homebrew.
+      Install it with npm:
+        npm install -g @usebruno/cli
+
+      fzf is optional, only for the interactive request and environment
+      pickers:
+        brew install fzf
+    EOS
+  end
+
+  test do
+    assert_match "run Bruno collection requests", shell_output("#{bin}/bru-run --help")
+  end
+end
